d414cb2ee63c95b94d1fd30dd27f319ad997acda
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-09-15 00:53:28 +0200

    Added test for case when registered handler is not valid (cannot resolve arguments)

---
+++ tests/InvalidCallableTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Awesomite\Nano;
+
+use Awesomite\Nano\ArgumentResolver\ArgumentResolverException;
+use Symfony\Component\HttpFoundation\Request;
+
+class InvalidCallableTest extends TestBase
+{
+    public function testGeneral()
+    {
+        $app = new Nano();
+        $app->get('home', '/', function ($redis) {
+            return 'Hello!';
+        });
+        $this->expectException(ArgumentResolverException::class);
+        $app->run(Request::create('http://home.local/'), false);
+    }
+}
